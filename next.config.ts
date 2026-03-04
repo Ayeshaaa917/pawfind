e7d7b1652a3d9c72e0f7d2049788b0239b5e046c
Ayesha <ayeshabianjizhe@gmail.com>
2026-03-04 11:31:37 +0500

    overwrite config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,1 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
-```
-
-Press **Ctrl + S** then in terminal:
-```
-git add .
-git commit -m "fix build errors"
-git push
+import type { NextConfig } from "next"; const nextConfig: NextConfig = { eslint: { ignoreDuringBuilds: true } }; export default nextConfig;
