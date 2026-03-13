35f0b6e83206f18b09cbabb73bf51e0491f06124
Ayeshaaa917 <ayeshabianjizhe@gmail.com>
2026-03-13 11:13:10 +0500

    Update rootProvider.tsx

--- app/rootProvider.tsx
+++ app/rootProvider.tsx
@@ -2,31 +2,16 @@
 
 import { ReactNode } from "react";
 import { base } from "wagmi/chains";
-import { WagmiProvider, createConfig, http } from "wagmi";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import "@coinbase/onchainkit/styles.css";
 
-const config = createConfig({
-  chains: [base],
-  transports: {
-    [base.id]: http(),
-  },
-});
-
-const queryClient = new QueryClient();
-
 export function RootProvider({ children }: { children: ReactNode }) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-          chain={base}
-        >
-          {children}
-        </OnchainKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <OnchainKitProvider
+      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      chain={base}
+    >
+      {children}
+    </OnchainKitProvider>
   );
 }
